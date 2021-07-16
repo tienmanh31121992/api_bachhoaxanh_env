59ee2df0ec84d6bc11eaeb7c17d934a187dd8041
tienmanh31121992 <tienmanh31121992@gmail.com>
2021-07-16 15:57:46 +0700

    Cau truc ung dung Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from config import Config
+from app.api import bp as api_bp
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
-
+flask_app.register_blueprint(api_bp, url_prefix='/api/v1.0')
 from app import routes, models
-from app.api import users, errors, tokens
--- app/api/users.py
+++ app/api/users.py
@@ -1,31 +1,31 @@
-from app import flask_app
+from app.api import bp
 
 
-@flask_app.route('/api/v1.0/users/<int:id>', methods=['GET'])
+@bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     pass
 
 
-@flask_app.route('/api/v1.0/users', methods=['GET'])
+@bp.route('/users', methods=['GET'])
 def get_users():
     return "Get users"
 
 
-@flask_app.route('/api/v1.0/users/<int:id>/followers', methods=['GET'])
+@bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
     pass
 
 
-@flask_app.route('/api/v1.0/users/<int:id>/followed', methods=['GET'])
+@bp.route('/users/<int:id>/followed', methods=['GET'])
 def get_followed(id):
     pass
 
 
-@flask_app.route('/api/v1.0/users', methods=['POST'])
+@bp.route('/users', methods=['POST'])
 def create_user():
     pass
 
 
-@flask_app.route('/api/v1.0/users/<int:id>', methods=['PUT'])
+@bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
     pass
